0d2fd7f8b8be7f82a16eb3b6e9c8b5c6da4f5f37
RichardlWilson <richard.l.wilson85@gmail.com>
2021-07-19 15:59:46 -0500

    updated gui design and layout

--- gui.py
+++ gui.py
@@ -9,17 +9,21 @@ gray_medium = '#4F4F51'
 gray_light = '#9D9FA2'
 
 root = Tk()
-root.title('proportion tool')
+root.geometry('630x540')
+root.title('proportion tool for Debra')
 root.configure(bg = gray)
 
-title_image = PhotoImage(file = 'resources/images/title_image.gif')
+#title_image = PhotoImage(file = 'resources/images/title_image.gif')
 
+sub_window = Frame(root, bg = gray, width = 610, height = 600, relief = 'solid', bd = 1)
+sub_window.grid(row = 0, column = 0, padx = 10, pady = 10)
 title_label = Label(
-    root,
-    image = title_image,
+    sub_window,
+    #image = title_image,
+    text = 'Proportion Tool', font = ('Times', 40),
     bg = gray,
     fg = orange,
-    padx = 100,
+    padx = 5,
     pady = 4
     )
 
@@ -28,23 +32,25 @@ title_label.grid(row = 0, column = 0)
 
 class Field:
     def __init__(self):
-        #option_list = ['inches', 'feet']
-        self.frame = Frame(root, bg = gray_medium, width = 400, height = 40)
-        self.label = Label(self.frame, bg = gray_medium, fg = orange, width = 20)
-        self.entry = Entry(self.frame, bg = gray_light, fg = 'white', width = 10)
+
+        self.frame = Frame(sub_window, bg = gray_medium, width = 600, height = 40)
+        self.label = Label(self.frame, bg = gray_medium, fg = orange, width = 20, font = ('Ariel', 15))
+        self.entry = Entry(self.frame, bg = gray_light, fg = 'white', width = 10, relief = 'flat', font = ('Ariel', 15))
 
         self.radio_var = IntVar()
-        self.radio_var.set('inches')
+        self.radio_var.set(1)
 
-        self.radio1 = Radiobutton(self.frame, text = 'inches', variable = self.radio_var, value = 0)
+        self.radio1 = Radiobutton(self.frame, text = 'inches', variable = self.radio_var, value = 1, width = 5, font = ('Ariel', 15))
         self.radio1.configure(bg = gray_medium, fg = orange)
+        #self.radio1.select()
 
-        self.radio2 = Radiobutton(self.frame,text = 'feet', variable = self.radio_var, value = 1)
+        self.radio2 = Radiobutton(self.frame, text = 'feet', variable = self.radio_var, value = 2, width = 5, font = ('Ariel', 15))
         self.radio2.configure(bg = gray_medium, fg = orange)
 
 
+
     def show(self, num = 1):
-        self.frame.grid(row = num, column = 0, padx = 100, pady = 10)
+        self.frame.grid(row = num, column = 0, padx = 5, pady = 10)
         self.frame.grid_propagate(0)
         self.label.grid(row = 0, column = 0, padx = 10, pady = 5)
         self.entry.grid(row = 0, column = 1, padx = 10, pady = 5)
@@ -59,22 +65,25 @@ def create_fields():
         field.show(num)
         num +=1
 
-    fields[0].label.configure(text = 'Height')
-    fields[1].label.configure(text = 'Width')
+    fields[0].label.configure(text = 'Current Height')
+    fields[1].label.configure(text = 'Current Width')
     fields[2].label.configure(text = 'Max Height')
     fields[3].label.configure(text = 'Max Width')
-    fields[4].label.configure(text = 'Max Sqaure Footage') 
+    fields[4].label.configure(text = 'Max Sqaure Footage')
+
+    return fields
 
 
 
 
 fields = create_fields()
-cal_button = Button(text = '• CALCULATE •', bg = gray_medium, fg = orange, width = 30, relief = 'flat')
+cal_button = Button(sub_window, text = '• CALCULATE •', bg = gray_medium, fg = orange, width = 30, relief = 'flat', font = ('Ariel', 15))
 cal_button.grid(row = 6, column = 0, pady = 10)
+return_lbl = Label(sub_window, bg = gray_medium, fg = orange, width = 50, height = 3, font = ('Ariel', 15))
+return_lbl.grid(row = 7, column = 0, pady = 5 )
 
+return_lbl.configure(text = 'Height:\nWidth:\nSquare Footage:', justify = 'left', anchor = 'w')
 
-# field = Field()
-# field.show()
 
 
 root.mainloop()
